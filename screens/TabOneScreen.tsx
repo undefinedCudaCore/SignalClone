4517047fbf449bdd042e8af96b87ec7e62d1de6b
undefinedCudaCore <teklithuania@gmail.com>
2022-03-07 14:40:49 +0200

    Start page basic style

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -5,12 +5,12 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image} />
-      <View>
+      <View style={styles.rightContainer}>
         <View style={styles.row}>
             <Text style={styles.name}>Elon Musk</Text>
             <Text style={styles.text}>11:11 AM</Text>
         </View> 
-        <Text style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, obcaecati? Eum laborum soluta temporibus vitae doloribus dignissimos, quaerat saepe quasi. Voluptatibus est, inventore illo natus repellat vitae architecto magnam temporibus.</Text>
+        <Text numberOfLines={1} style={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt odio maiores fugiat obcaecati recusandae veritatis eum inventore corporis porro ab impedit laudantium minus non quasi atque voluptatem, placeat vero esse.</Text>
       </View>
     </View>
   );
@@ -18,21 +18,30 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 
 const styles = StyleSheet.create({
   container: {
-
+    flexDirection: 'row',
+    padding: 6,
   },
   image: {
-    width: 100,
-    height: 100
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  rightContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
 
   },
   name: {
-
-  },
-  row: {
-
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 1,
   },
   text: {
-    fontSize: 30, 
-    color: 'blue'
+    color: 'grey',
   }
 });
